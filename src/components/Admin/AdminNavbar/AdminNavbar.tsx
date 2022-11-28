c7e27188729a3fcbf07fd6ff08d6ddf1ab3e0756
ytalogabr <ytalogabr@gmail.com>
2022-11-28 06:50:21 -0300

    Estilizando o menu de navegação

--- src/components/Admin/AdminNavbar/AdminNavbar.tsx
+++ src/components/Admin/AdminNavbar/AdminNavbar.tsx
@@ -1,14 +1,32 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import Navbar from '../../GlobalComponents/Navbar/Navbar'
+import styles from './AdminNavbar.module.css';
+
+import dashboard from '../../../assets/images/icons/dashboard.svg';
+import addcargo from '../../../assets/images/icons/addcargo.svg';
+import local from '../../../assets/images/icons/local.svg';
+import map from '../../../assets/images/icons/map.svg';
 
 const AdminNavbar = () => {
   return (
     <Navbar>
-      <Link to="/admin/dashboard">Painel de Controle</Link>
-      <Link to="/admin/cargo">Adicionar Carga</Link>
-      <Link to="/admin/tracker">Rastrear Carga</Link>
-      <Link to="/admin/cargomap">Mapa de Cargas</Link>
+      <NavLink to="/admin/dashboard" className={styles.link}>
+        <img src={dashboard} alt="Ícone de Dashboard" />
+        <span>Painel de Controle</span>
+      </NavLink>
+      <NavLink to="/admin/cargo" className={styles.link}>
+      <img src={addcargo} alt="Ícone de Caminhão" />
+        <span>Adicionar Carga</span>
+      </NavLink>
+      <NavLink to="/admin/tracker" className={styles.link}>
+      <img src={local} alt="Ícone de Localização" />
+        <span>Rastrear Carga</span>
+      </NavLink>
+      <NavLink to="/admin/cargomap" className={styles.link}>
+      <img src={map} alt="Ícone de Mapa" />
+        <span>Mapa de Cargas</span>
+      </NavLink>
     </Navbar>
   )
 }
--- src/components/GlobalComponents/Navbar/Navbar.tsx
+++ src/components/GlobalComponents/Navbar/Navbar.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import styles from './Navbar.module.css'
+import logo from '../../../assets/images/ship.svg';
 
 const Navbar = ({children}: any) => {
   return (
-    <nav className={styles.nav}>{children}</nav>
+    <nav className={styles.nav}>
+      <div className={styles.logoContainer}>
+        <img className={styles.logo} src={logo} alt="Imagem de Navio" />
+        <h1 className={styles.title}>Cargo<span>Tracker</span></h1>
+      </div>
+      {children}
+    </nav>
   )
 }
 
